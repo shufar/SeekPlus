214a8e70bc13795c2f6f0e8d0e0a4df15f8cc33f
Shubham Farkya <>
2024-02-21 17:02:30 +0800

    feat: SeekPlus app initial commit
    - Initial commit
    - Added the skeleton

---
+++ SeekPlus/SeekPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeekPlusApp.swift
+//  SeekPlus
+//
+//  Created by Shubh on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SeekPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
